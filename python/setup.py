65857dadab03087aefa179ea393758db46ae177e
Alexander Hiam <alex@graycat.io>
2015-10-23 17:21:19 -0400

    version to 1.0.3, rebuild docs

--- python/setup.py
+++ python/setup.py
@@ -13,14 +13,14 @@ extensions = [
   ]
 
 setup(name="serbus",
-      version="1.0.2",
+      version="1.0.3",
       description="A Python package for I2C and SPI serial bus communication on GNU/Linux systems.",
       long_description=open("../README.md").read(),
       author="Alexander Hiam",
       author_email="alex@graycat.io",
       license="MIT License",
       url="https://github.com/graycatlabs/serbus",
-      download_url="https://github.com/graycatlabs/serbus/tarball/v1.0.2",
+      download_url="https://github.com/graycatlabs/serbus/tarball/v1.0.3",
       keywords=["I2C", "SPI", "serial", "Linux"],
       packages=["serbus"],
       ext_modules=extensions, 
